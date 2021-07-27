ae950c1dd85c61966697eb9cf68d65112b9da7be
Paulo <pauloivocorrea@gmail.com>
2021-07-27 17:17:42 -0300

    robofriends v1

---
+++ robofriends/src/card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card.css';
+
+const Card = ({name,email,id}) => {
+    return (
+        <div className='cardTotal'>
+            <img src={`https://robohash.org/${id}' alt='robo`}/>
+            <div className='cardText'>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Card;
